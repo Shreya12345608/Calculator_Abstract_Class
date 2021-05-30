91c552383684f83398489a80def538e84624628f
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-30 21:43:03 +0530

    [Shreya]Add: Solve Using Abstract Class Using OverRide

---
+++ Calculator_Abstract_Class/Calculator_Abstract_Class/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Abstract_Class
+{
+        abstract class Calculate
+        {
+           public abstract void Multiply(int FirstNumber, int SecondNumber);
+        }
+
+        abstract class Multiplication : Calculate
+    {
+
+            public override void Multiply(int FirstNumber, int SecondNumber)
+            {
+                int Output = FirstNumber * SecondNumber;
+                Console.WriteLine("Calling Abstract  from Calculator class");
+                Console.WriteLine("Multiplication of " + FirstNumber +" * " + SecondNumber +  " = " + Output);
+            }
+
+            public abstract void Divibile(int FirstNumber, int SecondNumber);
+        }
+
+        class Division : Multiplication
+        {
+            public override void Divibile(int FirstNumber, int SecondNumber)
+            {
+                int Output = FirstNumber / SecondNumber;
+                Console.WriteLine(" Calling Abstract from Multiplication class");
+                Console.WriteLine($"Division of " + FirstNumber + " / " + SecondNumber + " = " + Output);
+            }
+        }
+    }
+
--- Calculator_Abstract_Class/Calculator_Abstract_Class/Program.cs
+++ Calculator_Abstract_Class/Calculator_Abstract_Class/Program.cs
@@ -10,7 +10,11 @@ namespace Calculator_Abstract_Class
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome Message for Calculator class");
+            Console.WriteLine("Welcome Message for Calculator Abstract Class");
+            Division division = new Division();
+            division.Multiply(25, 5);
+            division.Divibile(500, 10);
+            Console.ReadKey();
         }
     }
 }
